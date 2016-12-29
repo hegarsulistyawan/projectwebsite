0f8adbd062d9fa40a64f653a971da6ac5302aab7
Hegarujang\ujang <sulistyawanhegar@gmail.com>
2016-12-29 20:27:32 +0700

    iki rek

--- koneksi.php
+++ koneksi.php
@@ -5,5 +5,4 @@ $pass = "";	//password phpMyAdmin
 $name = "dbmhs";	//nama database
 
 $koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
-mysql_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
 ?>
