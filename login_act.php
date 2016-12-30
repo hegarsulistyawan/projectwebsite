14db2edd2ef9d8f0dbcf9234def56d62cff1ba5f
Dedi_kahfirohmatullah <dedikahfirohmatullah@gmail.com>
2016-12-30 21:55:36 +0700

    action login

--- login_act.php
+++ login_act.php
@@ -3,8 +3,8 @@ session_start();
 include 'admin/config.php';
 $uname=$_POST['uname'];
 $pass=$_POST['pass'];
-$pas=md5($pass);
-$query=mysql_query("select * from admin where uname='$uname' and pass='$pas'")or die(mysql_error());
+//$pas=md5($pass);
+$query=mysql_query("select * from admin where uname='$uname' and pass='$pass'")or die(mysql_error());
 if(mysql_num_rows($query)==1){
 	$_SESSION['uname']=$uname;
 	header("location:admin/index.php");
