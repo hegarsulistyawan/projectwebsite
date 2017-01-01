c73ec99bf08d8fb7ef4baeb82aad521fc9e20110
Hegarujang\ujang <sulistyawanhegar@gmail.com>
2017-01-02 02:06:47 +0700

    update

---
+++ barang.php
@@ -0,0 +1,94 @@
+<?php include 'header.php'; ?>
+<h3><span class="glyphicon glyphicon-briefcase"></span>  Data Obat</h3>
+<div class="col-md-12">
+	<button style="margin-bottom:20px" data-toggle="modal" data-target="#myModal" class="btn btn-info col-md-2"><span class="glyphicon glyphicon-plus"></span>Tambah Barang</button>
+	<a style="margin-bottom:10px" href="preview.php" target="_blank" class="btn btn-default pull-right"><span class='glyphicon glyphicon-print'></span>  Cetak</a>
+</div>
+<form action="cari_act.php" method="get">
+	<div class="input-group col-md-5 col-md-offset-7">
+		<span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-search"></span></span>
+		<input type="text" class="form-control" placeholder="Cari barang di sini .." aria-describedby="basic-addon1" name="cari">	
+	</div>
+</form>
+<br/>
+<table class="table table-hover">
+	<tr>
+		<th class="col-md-1">No</th>
+		<th class="col-md-2">Nama Barang</th>
+		<th class="col-md-2">Modal</th>
+		<th class="col-md-2">Harga Jual</th>
+		<th class="col-md-1">Jumlah</th>
+		<th class="col-md-3">Opsi</th>
+	</tr>
+	<?php 
+	if(isset($_GET['cari'])){
+		$cari=mysql_real_escape_string($_GET['cari']);
+		$brg=mysql_query("select * from barang where nama like '%$cari%' or jenis like '%$cari%'");
+	}else{
+		$brg=mysql_query("select * from barang");
+	}
+	$no=1;
+	while($b=mysql_fetch_array($brg)){
+
+		?>
+		<tr>
+			<td><?php echo $no++ ?></td>
+			<td><?php echo $b['nama'] ?></td>
+			<td>Rp.<?php echo number_format($b['modal']) ?>,-</td>
+			<td>Rp.<?php echo number_format($b['harga']) ?>,-</td>
+			<td><?php echo $b['jumlah'] ?></td>
+			<td>
+				<a href="det_barang.php?id=<?php echo $b['id']; ?>" class="btn btn-info">Detail</a>
+				<a href="edit.php?id=<?php echo $b['id']; ?>" class="btn btn-warning">Edit</a>
+				<a onclick="if(confirm('Apakah anda yakin ingin menghapus data ini ??')){ location.href='hapus.php?id=<?php echo $b['id']; ?>' }" class="btn btn-danger">Hapus</a>
+			</td>
+		</tr>		
+		<?php 
+	}
+	?>
+</table>
+
+<!-- modal input -->
+<div id="myModal" class="modal fade">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Tambah Barang Baru</h4>
+			</div>
+			<div class="modal-body">
+				<form action="tmb_brg_act.php" method="post">
+					<div class="form-group">
+						<label>Nama Barang</label>
+						<input name="nama" type="text" class="form-control" placeholder="Nama Barang ..">
+					</div>
+					<div class="form-group">
+						<label>Jenis</label>
+						<input name="jenis" type="text" class="form-control" placeholder="Jenis Barang ..">
+					</div>
+					<div class="form-group">
+						<label>Suplier</label>
+						<input name="suplier" type="text" class="form-control" placeholder="Suplier ..">
+					</div>
+					<div class="form-group">
+						<label>Harga Modal</label>
+						<input name="modal" type="text" class="form-control" placeholder="Modal per unit">
+					</div>	
+					<div class="form-group">
+						<label>Harga Jual</label>
+						<input name="harga" type="text" class="form-control" placeholder="Harga Jual per unit">
+					</div>	
+					<div class="form-group">
+						<label>Jumlah</label>
+						<input name="jumlah" type="text" class="form-control" placeholder="Jumlah">
+					</div>																	
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+					<input type="submit" class="btn btn-primary" value="Simpan">
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
