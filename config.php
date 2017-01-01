d520a209a12f7f2b7eb3967a80e37800be4d080a
dedikahfi <dedikahfirohmatullah@gmail.com>
2017-01-01 08:22:50 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,4 @@
 <?php
-$host = "localhost";	//nama host
-$user = "root";	//username phpMyAdmin
-$pass = "";	//password phpMyAdmin
-$name = "dbmhs";	//nama database
-
-$koneksi = mysql_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
+mysql_connect("localhost", "root", "");
+mysql_select_db("apotek_db");
 ?>
